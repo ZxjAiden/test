530543b28f2fc06d62cfa66ba4d69530e377eceb
ZxjAiden <z_xiao_jie@163.com>
2019-05-21 15:41:53 +0800

    项目初始化

---
+++ src/main/java/cn/example/test/controller/LoginController.java
@@ -0,0 +1,16 @@
+package cn.example.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiaojie.zhang
+ */
+@RestController
+public class LoginController {
+
+    @RequestMapping("/*")
+    public String hello(){
+        return "hello world";
+    }
+}
